0f62f4bc5dbcfa37ba94930c7c63d8c6f210f7ad
Renee Balmert <renee.balmert@immigrationlaw.com>
2016-11-16 17:37:36 -0800

    still building knit functions.

--- knitgit.py
+++ knitgit.py
@@ -4,7 +4,10 @@
 #github: tremlab
 #Hackbright Intro 2016
 
+######variable formatting/verifying functions
 
+
+######knitting functions
 ####TORSO
 
 def torso_start(trs_width, gauge_st, trs_length, gauge_r):		#all inputs floats
@@ -20,14 +23,14 @@ def torso_start(trs_width, gauge_st, trs_length, gauge_r):		#all inputs floats
 	print "REMINDER:  this pattern is in the round!\n"
 	print "Loop around from your last cast on, back to your first.\n"
 	print "Make sure to use a marker to keep track of where your row starts.\n"
-	print "Starting working in the pattern stitch you used to achieve your gauge.\n"
+	print "Start working in the pattern stitch you used to achieve your gauge.\n"
 	print "SAVOR THIS MOMENT!!! IT'S GONNA BE ***GORGEOUS!!!***\n"
 	print "Continue working in your chosen stitch until work measures %f(1) inches\n" (trs_length) #syntax???? 1 decimal!
 	print "(Should be about " + trs_rows + " rows, but do whatever feels right!)"
 	print "Continue in your pattern stitch for " + trs_half + " stitches.\n"
 	print "We're halfway!! Cast off " + trs_armhole + " stitches.\n"
 	print "Continue the row in your pattern stitch for " + trs_half + " stitches.\n"
-	print "Again, cast off " + trs_armhole + " stitches. Don't lost your marker! ;)"
+	print "Again, cast off " + trs_armhole + " stitches. Don't lose your marker! ;)"
 	print "It may not look like much, but you just finished the biggest piece of this sweater!  You **so** got this!!\n"
 	print "See? I TOLD you it would be gorgeous!! :*\n\n\n"
 	#return stiches???????????
@@ -39,27 +42,62 @@ def sleeve_start(trs_width, gauge_st, slv_length, gauge_r):				#all inputs float
 	#is zero?
 	slv_stitches = int(trs_width * 0.4)			#research this ratio!! shoudld be less than half of width.
 	slv_rows - int(slv_length * gauge_r)
-	trs_armhole_half = int((trs_stitches / 2) * 0.05)		#research this ratio!!! set max and min. (50% of armhole drop.)
-	trs_remainder = int(slv_stitches - (trs_armhole_half * 2))
+	trs_armhole = int((trs_stitches / 2) * 0.1)		#research this ratio!!! set max and min. 
+
 
 	print "But, let's not rest on our laurels just yet. ;)"
-	print "Set aside your (beautiful!) torso work, and lets get started on a sleeve."
+	print "Set aside your (beautiful!) torso work, and lets get started on a sleeve.\n"
 	print "Using the needles and yarn that you chose to achieve your gauge, cast on " + slv_stitches + " stitches.\n"
 	print "REMINDER: we're STILL working in the round. This is where you'll need a ninja star of double-pointed needles to work.\n"
 	print "(REMINDER: you're totally a ninja!!)\n"
+	print "Loop around from your last cast on, back to your first.\n"
 	print "Make sure to use a marker to keep track of where your row starts.\n"
-	print "Starting working in the pattern stitch you used to achieve your gauge.\n"
+	print "Start working in the pattern stitch you used to achieve your gauge.\n"
 	print "This is gonna go soooo much faster than the body! Rock your dance playlist.\n"
 	print "Continue working in your chosen stitch until work measures %f(1) inches\n" (slv_length) #syntax???? 1 decimal! 2?
 	print "(Should be about " + slv_rows + " rows, but do whatever feels right!)"
-	print "Cast off " + trs_armhole_half + " stitches.\n"
-	print "Continue in your pattern stitch for " + trs_remainder + " stitches.\n"	
-	print "Now, if we did our math right, there are " + trs_armhole_half + " +/- 1 stitches left. Cast 'em, off!\n"
-	print "Mark drop! Don't need that either.\n"
+	print "Continue in your pattern stitch until you reach the last " + trs_armhole + " stitches.\n"	
+	print "Cast off till the end of the row.\n"			
 	print "Now you have the body *AND* a sleeve. Time for chocolate. or zinfandel... or bourbon.\n\n\n"
 	print "But... we do need 2 sleeves. so repeat it all again, and make sure the lengths match each other exactly."
 
 ####NECK
+
+def neck_start():		#no calculations here. tricky just getting everything lined up.
+
+	print "The good news: you've finished more than half or your knitting! Go you!!"
+	print "The bad news: now it gets complicated.\n"
+	print "The pattern so far has been pretty straightforward and chill, but now you have to start paying attention.\n"
+	print "First, let's assemble all of our pieces into one big sweater-loop."
+	print "(REMINDER: you are Voltron!)\n"
+	print "You will need 3 markers, all the same color, and 1 in a distinct color to mark the absolute row beginning.\n"
+	print "Knit in your pattern stitch across the front of your torso, up to where you cast off for the first armhole.\n"
+	print "Insert the first of your 3 markers.\n"
+	print "Now bring your first sleeve, and line up the opening for the armhole, with the opening on the torso.\n"
+	print "Continue to knit across from the torso directly onto the sleeve.\n"
+	print "Knit all the way around the sleeve, and then insert the second of your 3 markers.\n"
+	print "Now continue to the back of your torso, knit all the way across to where you cast off for the seocnd armhole.\n"
+	print "Insert the last of your 3 markers.\n"
+	print "Bring your second sleeve, line up the armohole openings again, and knit around the armhole to the end.\n"
+	print "We're back at the begining, now in one continuous loop!\n"
+	print "Insert yur 4th marker - unique color - to mark the row start.\n"
+	print "Knitting this first row was probably very awkward, with your needles forced to turn around some pretty sharp curves.\n"
+	print "The next few rows will continue to be awkward, but with each row it will level out into an easier round shape, so hang in there.\n"
+
+
+def neck_finish(trs_stitches_oneside, sl_stitiches, gauge_st, gauge_r):
+	buffer_stitches = int(trs_stitches * 0.1) #???? calculate - definitely need max (10?) min(2)
+	final_stitch_count = int((trs_stitches + slv_stitches) * 2 * 0.3) ## .3?  calculate ratio, max/min.  front, back, 2 sleeves, then reduced to proportion.	
+
+	print "Now, for the remainder of the work, you will be methodically decreacing stitches, so the body will taper off to a neckline.\n"
+	print "You will need to pay attention to stitch and row count goign forward.\n"
+	print "Starting at the beginning of the row (your unique marker), knit " + buffer_stitches + " "
+
+
+
+
+
+
 
 
 
@@ -69,7 +107,10 @@ def main():
 	torso_start(trs_width, gauge_st, trs_length, gauge_r)
 
 	sleeve_start(trs_width, gauge_st, slv_length, gauge_r)
-#	neck_start.....
+
+	neck_start()
+
+	neck_finish()
 
 if __name__ == '__main__':
 	main()
